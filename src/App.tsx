cc32327d04c3df11379cf99def18c890e68b780a
jmj <waysoleward01@naver.com>
2025-05-26 02:19:13 +0900

    feat: tailwind 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="p-10 space-x-4 bg-white">
+      <Button>기본</Button>
+      <Button variant="outline">아웃라인</Button>
+      <Button variant="destructive">위험</Button>
+      <div className="bg-red-500 text-white p-4">Tailwind 됨?</div>
+    </div>
+  );
 }
-
-export default App
