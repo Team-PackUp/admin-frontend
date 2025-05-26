b106ea2fd72d27dca1f7eeed060e1f3baac85f20
jmj <waysoleward01@naver.com>
2025-05-26 13:31:07 +0900

    feat: env 관련 설정

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 import { attachAuthInterceptors } from "./interceptors";
 
+const baseURL = import.meta.env.VITE_BASE_URL;
+
 export const apiClient = axios.create({
-  baseURL: `${process.env.BASE_URL}`,
+  baseURL,
   headers: {
     "Content-type": "application/json",
   },
 });
 
 export const apiFilesClient = axios.create({
-  baseURL: `${process.env.BASE_URL}`,
+  baseURL,
   headers: {
     "Content-Type": "multipart/form-data",
   },
