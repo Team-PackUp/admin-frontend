24c47e6de26f5ad7e57db149efd63b1a72248f3d
jmj <waysoleward01@naver.com>
2025-06-25 03:10:37 +0900

    feat: credential은 로그인 요청할 때 설정

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,6 +2,8 @@ import { apiClient } from "./apiClient";
 import type { LoginRequest, LoginResponse } from "@/@types/auth.types";
 
 export const login = async (data: LoginRequest): Promise<LoginResponse> => {
-  const response = await apiClient.post("/auth/login", data);
+  const response = await apiClient.post("/auth/login", data, {
+    withCredentials: true,
+  });
   return response.data;
 };
