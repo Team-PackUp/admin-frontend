474801abf0658365c5c66dcf47bcc2ab34b19210
jmj <waysoleward01@naver.com>
2025-07-09 21:03:09 +0900

    feat: qs 라이브러리 추가 - axios 직렬화 이슈 해결

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import qs from "qs";
 import { attachAuthInterceptors } from "./interceptors";
 
 const baseURL = import.meta.env.VITE_BASE_URL;
@@ -7,6 +8,9 @@ export const apiClient = axios.create({
   baseURL,
   headers: {
     "Content-type": "application/json",
+  },
+  paramsSerializer: {
+    serialize: (params) => qs.stringify(params, { arrayFormat: "repeat" }), // ✅ 핵심!
   },
 });
 
